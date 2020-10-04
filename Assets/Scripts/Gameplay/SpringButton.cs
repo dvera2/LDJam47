89f726ac04de7958445ae2e5bc1f8475b2295605
Dave Vera <dvera2@gmail.com>
2020-10-03 18:37:27 -0700

    Integrated puppy and hairdryer art

--- Assets/Scripts/Gameplay/SpringButton.cs
+++ Assets/Scripts/Gameplay/SpringButton.cs
@@ -7,19 +7,21 @@ public class SpringButton : MonoBehaviour
     public float Force = 1.0f;
     public ForceMode2D ForceMode = ForceMode2D.Impulse;
     
-    public Transform SpringButtonSprite;
-
-    private Vector3 _localSpringButton;
-    private Vector3 _offset;
+    public SpriteRenderer SpringButtonSprite;
+    public Sprite NeutralPuppy;
+    public Sprite ActivatedPuppy;
 
     private void Awake()
     {
-        _localSpringButton = SpringButtonSprite.localPosition;
+        SpringButtonSprite.sprite = NeutralPuppy;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.attachedRigidbody)
+            return;
+
+        if (!collision.gameObject.CompareTag("Hamster"))
             return;
 
         collision.attachedRigidbody.AddForce(transform.TransformDirection(Vector2.up) * Force, ForceMode);
@@ -28,27 +30,10 @@ public class SpringButton : MonoBehaviour
 
     private IEnumerator DoSpringTrigger()
     {
-        _offset = Vector3.zero;
-        float t = 0;
-        float time = 0;
-        float duration = 0.1f;
-        while( time < duration )
-        {
-            time += Time.deltaTime;
-            t = time / duration;
-            _offset.y = Mathf.Lerp(0, 0.1f, t);
-            SpringButtonSprite.localPosition = _localSpringButton + _offset;
-            yield return new WaitForEndOfFrame();
-        }
-        t = 0;
-        time = 0;
-        while (time < duration)
-        {
-            time += Time.deltaTime;
-            t = time / duration;
-            _offset.y = Mathf.Lerp(0.1f, 0f, t);
-            SpringButtonSprite.localPosition = _localSpringButton + _offset;
-            yield return new WaitForEndOfFrame();
-        }
+        SpringButtonSprite.sprite = ActivatedPuppy;
+
+        yield return new WaitForSeconds(0.5f);
+
+        SpringButtonSprite.sprite = NeutralPuppy;
     }
 }
