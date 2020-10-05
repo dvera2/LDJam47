e39c05c474235071301ac7a1d95293a4cca89acf
Dave Vera <dvera2@gmail.com>
2020-10-04 21:17:04 -0700

    WIP level list

---
+++ Assets/Scripts/System/LevelList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[System.Serializable]
+public class LevelEntry
+{
+#if UNITY_EDITOR
+    public SceneAsset Asset;
+#endif
+    public string SceneName;
+}
+
+[CreateAssetMenu(fileName = "Level Progression", menuName = "System/Level Progression")]
+public class LevelList : ScriptableObject
+{
+    public List<LevelEntry> Levels = new List<LevelEntry>();
+
+    private void OnValidate()
+    {
+        foreach(var l in Levels)
+        {
+            if (l.Asset)
+                l.SceneName = l.Asset.name;
+        }
+    }
+}
